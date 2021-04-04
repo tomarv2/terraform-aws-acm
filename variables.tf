fd4e798253d5df042384e1d624701e7832781c57
tomarv2 <varuntomar2019@gmail.com>
2021-04-03 20:09:05 -0700

    formatting

--- variables.tf
+++ variables.tf
@@ -15,8 +15,9 @@ variable "profile_to_use" {
 }
 
 variable "aws_region" {
-  type    = string
-  default = "us-west-2"
+  description = "default aws region"
+  type        = string
+  default     = "us-west-2"
 }
 
 variable "create_certificate" {
