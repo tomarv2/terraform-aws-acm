bd4e6c1f05b8055776911b62aec06ac79b087aa3
tomarv2 <varuntomar2019@gmail.com>
2021-04-03 20:03:47 -0700

    formatting

--- variables.tf
+++ variables.tf
@@ -69,38 +69,7 @@ variable "zone_id" {
   default     = ""
 }
 
-//variable "tags" {
-//  description = "A mapping of tags to assign to the resource"
-//  type        = map(string)
-//  default     = {}
-//}
-
 variable "dns_ttl" {
   description = "The TTL of DNS recursive resolvers to cache information about this record."
   default     = 60
 }
-
-#
-//variable "types" {
-//  type        = list(any)
-//  default     = []
-//  description = "The record type. Valid values are A, AAAA, CAA, CNAME, MX, NAPTR, NS, PTR, SOA, SPF, SRV and TXT. "
-//}
-
-//variable "ttls" {
-//  type        = list(any)
-//  default     = []
-//  description = "(Required for non-alias records) The TTL of the record."
-//}
-//
-//variable "names" {
-//  type        = list(any)
-//  default     = []
-//  description = "The name of the record."
-//}
-
-//variable "values" {
-//  type        = list(any)
-//  default     = []
-//  description = "(Required for non-alias records) A string list of records. To specify a single record value longer than 255 characters such as a TXT record for DKIM, add \"\" inside the Terraform configuration string (e.g. \"first255characters\"\"morecharacters\")."
-//}
