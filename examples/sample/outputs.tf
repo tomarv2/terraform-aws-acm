89a6c2fcb00b5721b6c457b060ca5bf5d893310d
tomarv2 <varuntomar2019@gmail.com>
2021-04-03 20:14:30 -0700

    formatting

--- examples/sample/outputs.tf
+++ examples/sample/outputs.tf
@@ -24,35 +24,43 @@ output "validation_record_name" {
 # ACM
 # ---------------------------------------------
 output "certificate_body" {
-  value = module.acm.certificate_body
+  description = "Certificate body"
+  value       = module.acm.certificate_body
 }
 
 output "certificate_domain_name" {
-  value = module.acm.certificate_domain_name
+  description = "Certificate domain name"
+  value       = module.acm.certificate_domain_name
 }
 
 output "certificate_domain_validation_options" {
-  value = module.acm.certificate_domain_validation_options
+  description = "Certificate validation options"
+  value       = module.acm.certificate_domain_validation_options
 }
 
 output "certificate_arn" {
-  value = module.acm.certificate_arn
+  description = "Certificate ARN"
+  value       = module.acm.certificate_arn
 }
 
 output "certificate_san" {
-  value = module.acm.certificate_san
+  description = "Certificate SAN"
+  value       = module.acm.certificate_san
 }
 
 output "certificate_validation_email" {
-  value = module.acm.certificate_validation_email
+  description = "Certificate validation email"
+  value       = module.acm.certificate_validation_email
 }
 # ---------------------------------------------
 # ACM validation
 # ---------------------------------------------
 output "certificate_validation_record_fqdns" {
-  value = module.acm.certificate_validation_record_fqdns
+  description = "Certificate validation fqdn"
+  value       = module.acm.certificate_validation_record_fqdns
 }
 
 output "certificate_validation_id" {
-  value = module.acm.certificate_validation_id
+  description = "Certificate validation id"
+  value       = module.acm.certificate_validation_id
 }
